ac9dfd441bbb2a9cb028f2183b597f4990aefe85
Karen Paz <mabelpazf@gmail.com>
2020-10-25 16:31:14 -0600

    Agregando ejercicio propiedades

---
+++ Ejercicios/Propiedades/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Pedro";
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+        }
+    }
+}
